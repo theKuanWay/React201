97dc490d1e4b2084293d6a5dc444ec3355c9a2ef
theKuanWay <liewkw94@gmail.com>
2022-08-03 22:01:36 +0800

    using pc

--- moviebrowser/src/App.js
+++ moviebrowser/src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import NavBar from './Components/NavBar/NavBar';
 import Home from './Components/Home/Home';
 import AboutView from './Components/About/About';
+import { Switch,Route} from 'react-router-dom';
 
 function App() {
   return (
     <div>
-    <NavBar />
-    <Home />
-    <AboutView />
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={AboutView} />
+      </Switch>
     </div>
   );
 }
