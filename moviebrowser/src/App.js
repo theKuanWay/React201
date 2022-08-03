bc631bde6eac073fa845d719830bab3a9111499f
theKuanWay <liewkw94@gmail.com>
2022-08-03 22:23:39 +0800

    add in routes

--- moviebrowser/src/App.js
+++ moviebrowser/src/App.js
@@ -2,16 +2,16 @@ import './App.css';
 import NavBar from './Components/NavBar/NavBar';
 import Home from './Components/Home/Home';
 import AboutView from './Components/About/About';
-import { Switch,Route} from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={AboutView} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<AboutView/>} />
+      </Routes>
     </div>
   );
 }
