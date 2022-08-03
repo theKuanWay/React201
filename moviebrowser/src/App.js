7801bcea44dfbcb546e00c54543d042e4a682a7b
theKuanWay <liewkw94@gmail.com>
2022-08-03 14:29:39 +0800

    add home component

--- moviebrowser/src/App.js
+++ moviebrowser/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NavBar from './Components/NavBar/NavBar';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
     <div>
     <NavBar />
+    <Home />
     </div>
   );
 }
