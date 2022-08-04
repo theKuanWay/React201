d2d9318b554d673fb78da22995b2d1e4b3ea3666
theKuanWay <liewkw94@gmail.com>
2022-08-04 17:22:50 +0800

    get API key from website

--- moviebrowser/src/Components/Search/SearchView.js
+++ moviebrowser/src/Components/Search/SearchView.js
@@ -1,6 +1,10 @@
 import React from "react";
 import Hero from "../Hero/Hero";
 
+//API key = ff17f76a0910d30be29baf43308e80be
+//search movie url = 
+https://api.themoviedb.org/3/search/movie?api_key=ff17f76a0910d30be29baf43308e80be&language=en-US&query=star%20wars&page=1&include_adult=false
+
 const SearchView = ({keyword, searchResults }) => {
     const title = `You are searching for ${keyword}`
     console.log(searchResults, " are the search results")
