c0e1f34518394050dde3affa849f82a5fa6abdc0
theKuanWay <liewkw94@gmail.com>
2022-08-05 11:41:58 +0800

    format code style

--- moviebrowser/src/Components/Search/SearchView.js
+++ moviebrowser/src/Components/Search/SearchView.js
@@ -4,40 +4,44 @@ import Hero from "../Hero/Hero";
 //API key = ff17f76a0910d30be29baf43308e80be
 //search movie url = https://api.themoviedb.org/3/search/movie?api_key=ff17f76a0910d30be29baf43308e80be&language=en-US&query=star%20wars&page=1&include_adult=false
 
-const MovieCard = ({movie}) => {
-    const posterUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`
-    return (
-        <div className="col-lg-3 col-md-3 col-2 my-4">
-        <div className="card">
-        <img src={posterUrl} className="card-img-top" alt={movie.original_title} />
+const MovieCard = ({ movie }) => {
+  const posterUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+  return (
+    <div className="col-lg-3 col-md-3 col-2 my-4">
+      <div className="card">
+        <img
+          src={posterUrl}
+          className="card-img-top"
+          alt={movie.original_title}
+        />
         <div className="card-body">
-        <h5 className="card-title">{movie.original_title}</h5>
-        <a href="#" className="btn btn-primary">Show details</a>
+          <h5 className="card-title">{movie.original_title}</h5>
+          <a href="#" className="btn btn-primary">
+            Show details
+          </a>
         </div>
-        </div>
-        </div> 
-  )
-}
+      </div>
+    </div>
+  );
+};
 
 const SearchView = ({ keyword, searchResults }) => {
-    const title = `You are searching for ${keyword}`
+  const title = `You are searching for ${keyword}`;
 
-    const resultsHtml = searchResults.map((obj, i) => {
-        return <MovieCard movie={obj} key={i} />
-    }); 
+  const resultsHtml = searchResults.map((obj, i) => {
+    return <MovieCard movie={obj} key={i} />;
+  });
 
-    return (
+  return (
     <>
-    <Hero text={title}/>
-    {resultsHtml &&
-    <div className="container">
-        <div className="row">
-            {resultsHtml}
+      <Hero text={title} />
+      {resultsHtml && (
+        <div className="container">
+          <div className="row">{resultsHtml}</div>
         </div>
-    </div>
-    }
+      )}
     </>
-    );
-}
+  );
+};
 
 export default SearchView;
