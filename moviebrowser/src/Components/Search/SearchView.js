740895f067a18f49016777b0327dd7c06fda87a0
theKuanWay <liewkw94@gmail.com>
2022-08-05 10:51:27 +0800

    add in bootstrap 5 search results

--- moviebrowser/src/Components/Search/SearchView.js
+++ moviebrowser/src/Components/Search/SearchView.js
@@ -5,15 +5,17 @@ import Hero from "../Hero/Hero";
 //search movie url = https://api.themoviedb.org/3/search/movie?api_key=ff17f76a0910d30be29baf43308e80be&language=en-US&query=star%20wars&page=1&include_adult=false
 
 const MovieCard = ({movie}) => {
+    const posterUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`
     return (
-        <div className="card" style="width: 18rem;">
-        <img src="..." className="card-img-top" alt="..." />
+        <div className="col-lg-3 col-md-3 col-2 my-4">
+        <div className="card">
+        <img src={posterUrl} className="card-img-top" alt={movie.original_title} />
         <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" className="btn btn-primary">Go somewhere</a>
+        <h5 className="card-title">{movie.original_title}</h5>
+        <a href="#" className="btn btn-primary">Show details</a>
         </div>
-    </div>
+        </div>
+        </div> 
   )
 }
 
@@ -21,13 +23,19 @@ const SearchView = ({ keyword, searchResults }) => {
     const title = `You are searching for ${keyword}`
 
     const resultsHtml = searchResults.map((obj, i) => {
-        return <div key={i}>{obj.original_title}</div>
+        return <MovieCard movie={obj} key={i} />
     }); 
 
     return (
     <>
     <Hero text={title}/>
-    {resultsHtml}
+    {resultsHtml &&
+    <div className="container">
+        <div className="row">
+            {resultsHtml}
+        </div>
+    </div>
+    }
     </>
     );
 }
