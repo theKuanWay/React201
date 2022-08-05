49908510b42a154899749c4759ecb95b560c145f
theKuanWay <liewkw94@gmail.com>
2022-08-05 09:37:44 +0800

    add use effect for search text

--- moviebrowser/src/App.js
+++ moviebrowser/src/App.js
@@ -12,6 +12,17 @@ function App() {
   const [searchResults, setSearchResults] = useState ({});
   const [searchText, setSearchText] = useState ('');
 
+  useEffect (()=> {
+      console.log (searchText, "is the search text")
+      fetch(`https://api.themoviedb.org/3/search/movie?api_key=ff17f76a0910d30be29baf43308e80be&language=en-US&query=${searchText}&page=1&include_adult=false
+      `)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        setSearchResults(data.results)
+      })
+  }, [searchText])
+
   return (
     <div>
       <NavBar searchText={searchText} setSearchText={setSearchText}/>
--- moviebrowser/src/Components/Search/SearchView.js
+++ moviebrowser/src/Components/Search/SearchView.js
@@ -2,8 +2,7 @@ import React from "react";
 import Hero from "../Hero/Hero";
 
 //API key = ff17f76a0910d30be29baf43308e80be
-//search movie url = 
-https://api.themoviedb.org/3/search/movie?api_key=ff17f76a0910d30be29baf43308e80be&language=en-US&query=star%20wars&page=1&include_adult=false
+//search movie url = https://api.themoviedb.org/3/search/movie?api_key=ff17f76a0910d30be29baf43308e80be&language=en-US&query=star%20wars&page=1&include_adult=false
 
 const SearchView = ({keyword, searchResults }) => {
     const title = `You are searching for ${keyword}`
