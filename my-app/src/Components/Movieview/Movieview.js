4dbc2207624c17e9dfd2774cdf5aa75a1f3d5b01
theKuanWay <liewkw94@gmail.com>
2022-08-05 15:48:48 +0800

    add movie details component

--- my-app/src/Components/Movieview/Movieview.js
+++ my-app/src/Components/Movieview/Movieview.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
+import Hero from "../Hero/Hero";
+import { useEffect } from "react";
 
 const MovieView = () => {
   const { id } = useParams();
-  console.log(id);
+  const [movieDetails, setMovieDetails] = useState({});
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}?api_key=ff17f76a0910d30be29baf43308e80be&language=en-US`
+    )
+      .then((response) => response.json)
+      .then((data) => {
+        setMovieDetails(data);
+      });
+  }, [id]);
 
   return (
     <>
-      <div></div>
+      <Hero text={movieDetails.original.title} />
     </>
   );
 };
--- my-app/src/Components/Search/SearchView.js
+++ my-app/src/Components/Search/SearchView.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Hero from "../Hero/Hero";
+import { Link } from "react-router-dom";
 
 //API key = ff17f76a0910d30be29baf43308e80be
 //search movie url = https://api.themoviedb.org/3/search/movie?api_key=ff17f76a0910d30be29baf43308e80be&language=en-US&query=star%20wars&page=1&include_adult=false
 
 const MovieCard = ({ movie }) => {
   const posterUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+  const detailUrl = `/movies/${movie.id}`;
+
   return (
     <div className="col-lg-3 col-md-3 col-2 my-4">
       <div className="card">
@@ -16,9 +19,9 @@ const MovieCard = ({ movie }) => {
         />
         <div className="card-body">
           <h5 className="card-title">{movie.original_title}</h5>
-          <a href="#" className="btn btn-primary">
+          <Link to={detailUrl} className="btn btn-primary">
             Show details
-          </a>
+          </Link>
         </div>
       </div>
     </div>
